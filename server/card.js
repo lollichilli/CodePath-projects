4f55b4b08eac08e4f522d8b2820a08aa9c76f6e0
lollichilli <nguyenthanhtunghp2003@gmail.com>
2023-03-04 16:21:38 -0600

    My first commit

---
+++ server/card.js
@@ -0,0 +1,91 @@
+// Image credits: Kickstarter
+const cards =  [
+    {
+    id: 1,
+    level: "Easy",
+    question: "What is the 2nd planet in the solar system?",
+    answer: 'Venus',
+   
+    
+    },
+
+
+    {
+    id: 2,
+    level: "Easy",
+    question: "What is the satellite of the earth?",
+    answer: 'The moon',
+
+    },
+
+
+    {
+    id: 3,
+    level: "Easy",
+    question: "What is the 8th planet from the sun?",
+    answer: "Neptune",
+    },
+
+
+    {
+    id: 4,
+    level: "Medium",
+    question: "Can we live on Mercury planet?",
+    answer: "The harsh conditions on the planet's surface and the tenuous atmosphere make it impossible for any life form known to man to exist",
+    },
+
+
+    {
+    id: 5,
+    level:  "Medium",
+    question: "Can humans breathe on Mars?",
+    answer: "Mars does have an atmosphere, but it is about 100 times thinner than Earth's atmosphere and it has very little oxygen",
+   
+    },
+
+    {
+    id: 6,
+    level: "Medium",
+    question: "Is Mars hot or cold",
+    answer: "Mars is generally considered a cold planet, with average temperatures that are much colder than Earth's",
+    
+    },
+
+    {
+    id: 7,
+    level: "Medium",
+    question: "Is Uranus full of diamonds?",
+    answer: "Yes. High pressure experiments suggest large amounts of diamonds are formed from methane on the ice giant planets Uranus and Neptune",
+    
+    },
+
+    {
+    id: 8,
+    level: "Hard",
+    question: "What gas makes up the majority of Jupiter's atmosphere?",
+    answer: "The gas that makes up the majority of Jupiter's atmosphere is hydrogen (75%)",
+    
+    },
+
+    {
+    id: 9,
+    level: "Hard",
+    question: "What is the name of the largest and most well-known ring system surrounding Saturn?",
+    answer: 'It is is the "A ring" or the "Outer Ring".',
+   
+    },
+
+    {
+    id: 10,
+    level: "Hard",
+    question: "What is the name of the spacecraft that orbited Saturn from 2004 to 2017?",
+    answer: 'It is is the "A ring" or the "Outer Ring"',
+    
+    }
+
+   
+]
+
+
+  
+  export default cards;
